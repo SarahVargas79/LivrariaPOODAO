a38e77c49445a44da62329f7e50a0e40b1876791
Sarah Rafaela Vargas de Andrades <182010049@senacinformatica.br>
2023-04-05 11:58:54 -0300

    DAOFactory e clientes serviços commit

--- src/dao/ClienteDAO.java
+++ src/dao/ClienteDAO.java
@@ -7,12 +7,11 @@ package dao;
 
 import conexao.Conexao;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import model.Cliente;
 
 /**
@@ -23,24 +22,26 @@ public class ClienteDAO {
 
     public void cadastrarClienteDAO(Cliente cVO) {
         //busca conexão com o BD
-        Connection con = Conexao.getConexao();
+        Connection con = Conexao.getConexao();//abrindo conexão
         try {
             //cria espaço de trabalho SQL, é a área no Java onde vamo executar os sripts SQL.
-            Statement stat = con.createStatement();
             String sql;
-            sql = "insert into clientes values" 
-                                                        + "(null, " 
-                                                        + "'" + cVO.getNomeCliente() + "'," 
-                                                        + "'" + cVO.getCpf() + "');";
-            stat.execute(sql);
+            sql = "insert into clientes values (null, ?, ?, null, ?, ?)";//? paramêtros que está esperando para receber, null é o id.
+            PreparedStatement pst = con.prepareStatement(sql);
+            //Estes são os paramêtros
+            pst.setString(1, cVO.getNomeCliente());
+            pst.setString(2, cVO.getCpf());
+            pst.setString(3, cVO.getEndereco());
+            pst.setString(4, cVO.getTelefone());
+            pst.executeUpdate();
         } catch (SQLException ex) {
             System.out.println("\nErro ao Cadastrar!\n" + ex.getMessage());        
         }
     }//fim cadastrar
     
-    public ArrayList<Cliente> getClienteBD() {
+    public ArrayList<Cliente> getClientesDAO() {
         Connection con = Conexao.getConexao();
-        try {//Para tratr erros
+        try {//Para tratar erros
             Statement stat = con.createStatement();
             String sql = "select * from clientes";//' para executar para o banco(mysql)
             ResultSet rs = stat.executeQuery(sql);//ResultSet estrutura no java, é um meio de campo entre o banco de dados e o java(aplicação).
@@ -51,6 +52,8 @@ public class ClienteDAO {
                 c.setIdCliente(rs.getByte("idcliente"));
                 c.setNomeCliente(rs.getString("nome"));
                 c.setCpf(rs.getString("cpf"));
+                c.setEndereco(rs.getString("endereco"));
+                c.setTelefone(rs.getString("telefone"));
                 clientes.add(c);
             }
             return clientes;
@@ -64,14 +67,17 @@ public class ClienteDAO {
         Connection con = Conexao.getConexao();//estabelecendo conexão
         Cliente c = null;
         try {
-            Statement stat = con.createStatement();
-            String sql = "select * from clientes where cpf = '" + cpf + "'";
-            ResultSet rs = stat.executeQuery(sql);
+            String sql = "select * from clientes where cpf = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, cpf);
+            ResultSet rs = pst.executeQuery(sql);
             while (rs.next()) {              
                 //lado do java |x| (lado do banco)
                 c.setIdCliente(rs.getByte("idcliente"));
                 c.setNomeCliente(rs.getString("nome"));
                 c.setCpf(rs.getString("cpf"));
+                c.setEndereco(rs.getString("endereco"));
+                c.setTelefone(rs.getString("telefone"));
             }
         } catch (SQLException ex) {
             System.out.println("\nErro ao consultar CPF!\n" + ex.getMessage());
@@ -79,4 +85,30 @@ public class ClienteDAO {
        return c;
     }
     
+    public void deletarClienteDAO(String cpf) {
+        Connection con = Conexao.getConexao();
+        try {
+            String sql = "delete from clientes where cpf = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, cpf);
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("\nErro ao deletar CPF!\n" + ex.getMessage());
+        }
+    }//fim deletarCliente
+    
+    public void atualizaClienteByDoc(Cliente cVO) {
+        Connection con = Conexao.getConexao();
+        try {
+            String sql = "update clientes set nome = ?, endereco = ?, telefone = ? where cpf = ?";
+            PreparedStatement pst = con.prepareStatement(sql);
+            pst.setString(1, cVO.getNomeCliente());
+            pst.setString(2, cVO.getEndereco());
+            pst.setString(3, cVO.getTelefone());
+            pst.setString(4, cVO.getCpf());
+            pst.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("\nErro ao atualizar CPF!\n" + ex.getMessage());
+        }
+    }
 }
